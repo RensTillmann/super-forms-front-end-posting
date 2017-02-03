0a15411cc3c781e55b7f90fac9483ba4491eb061
feeling4design <info@feeling4design.nl>
2017-02-04 00:30:51 +0100

    - Added: Automatic updates

--- super-forms-front-end-posting.php
+++ super-forms-front-end-posting.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Super Forms Front-end Posting
+ * Super Forms - Front-end Posting
  *
- * @package   Super Forms Front-end Posting
+ * @package   Super Forms - Front-end Posting
  * @author    feeling4design
  * @link      http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * @copyright 2015 by feeling4design
  *
  * @wordpress-plugin
- * Plugin Name: Super Forms Front-end Posting
+ * Plugin Name: Super Forms - Front-end Posting
  * Plugin URI:  http://codecanyon.net/item/super-forms-drag-drop-form-builder/13979866
  * Description: Let visitors create posts from your front-end website
- * Version:     1.0.2
+ * Version:     1.1.0
  * Author:      feeling4design
  * Author URI:  http://codecanyon.net/user/feeling4design
 */
@@ -36,9 +36,18 @@ if(!class_exists('SUPER_Frontend_Posting')) :
          *
          *	@since		1.0.0
         */
-        public $version = '1.0.2';
+        public $version = '1.1.0';
 
         
+        /**
+         * @var string
+         *
+         *  @since      1.1.0
+        */
+        public $add_on_slug = 'frontend_posting';
+        public $add_on_name = 'Front-end Posting';
+
+
         /**
          * @var SUPER_Frontend_Posting The single instance of the class
          *
@@ -121,6 +130,12 @@ if(!class_exists('SUPER_Frontend_Posting')) :
         */
         private function init_hooks() {
             
+            // @since 1.1.0
+            register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+            // Filters since 1.1.0
+            add_filter( 'super_after_activation_message_filter', array( $this, 'activation_message' ), 10, 2 );
+
+
             if ( $this->is_request( 'frontend' ) ) {
                 
                 // Filters since 1.0.0
@@ -135,7 +150,11 @@ if(!class_exists('SUPER_Frontend_Posting')) :
                 add_filter( 'super_settings_after_smtp_server_filter', array( $this, 'add_settings' ), 10, 2 );
                 add_filter( 'super_shortcodes_after_form_elements_filter', array( $this, 'add_text_field_settings' ), 10, 2 );
 
-                // Actions since 1.0.0
+                // Filters since 1.1.0
+                add_filter( 'super_settings_end_filter', array( $this, 'activation' ), 100, 2 );
+                
+                // Actions since 1.1.0
+                add_action( 'init', array( $this, 'update_plugin' ) );
 
             }
             
@@ -149,6 +168,67 @@ if(!class_exists('SUPER_Frontend_Posting')) :
             }
             
         }
+
+
+        /**
+         * Automatically update plugin from the repository
+         *
+         *  @since      1.1.0
+        */
+        function update_plugin() {
+            if( defined('SUPER_PLUGIN_DIR') ) {
+                require_once ( SUPER_PLUGIN_DIR . '/includes/admin/update-super-forms.php' );
+                $plugin_remote_path = 'http://f4d.nl/super-forms/';
+                $plugin_slug = plugin_basename( __FILE__ );
+                new SUPER_WP_AutoUpdate( $this->version, $plugin_remote_path, $plugin_slug, '', '', $this->add_on_slug );
+            }
+        }
+
+
+        /**
+         * Add the activation under the "Activate" TAB
+         * 
+         * @since       1.1.0
+        */
+        public function activation($array, $data) {
+            if (method_exists('SUPER_Forms','add_on_activation')) {
+                return SUPER_Forms::add_on_activation($array, $this->add_on_slug, $this->add_on_name);
+            }else{
+                return $array;
+            }
+        }
+
+
+        /**  
+         *  Deactivate
+         *
+         *  Upon plugin deactivation delete activation
+         *
+         *  @since      1.1.0
+         */
+        public static function deactivate(){
+            if (method_exists('SUPER_Forms','add_on_deactivate')) {
+                SUPER_Forms::add_on_deactivate(SUPER_Frontend_Posting()->add_on_slug);
+            }
+        }
+
+
+        /**
+         * Check license and show activation message
+         * 
+         * @since       1.1.0
+        */
+        public function activation_message( $activation_msg, $data ) {
+            if (method_exists('SUPER_Forms','add_on_activation_message')) {
+                $form_id = absint($data['id']);
+                $settings = $data['settings'];
+                if( (isset($settings['frontend_posting_action'])) && ($settings['frontend_posting_action']!='none') ) {
+                    return SUPER_Forms::add_on_activation_message($activation_msg, $this->add_on_slug, $this->add_on_name);
+                }
+            }
+            return $activation_msg;
+        }
+
 
         /**
          * Hook into settings and add Text field settings
@@ -678,8 +758,23 @@ if(!class_exists('SUPER_Frontend_Posting')) :
                     $custom_meta = explode( "\n", $settings['frontend_posting_meta'] );
                     foreach( $custom_meta as $k ) {
                         $field = explode( "|", $k );
+
+                        // @since 1.0.3 - first check if a field with the name exists
                         if( isset( $data[$field[0]]['value'] ) ) {
                             $meta_data[$field[1]] = $data[$field[0]]['value'];
+                        }else{
+                            
+                            // @since 1.0.3 - if no field exists, just save it as a string
+                            $string = SUPER_Common::email_tags( $field[0], $data, $settings );
+                            
+                            // @since 1.0.3 - check if string is serialized array
+                            $unserialize = @unserialize($string);
+                            if ($unserialize !== false) {
+                                $meta_data[$field[1]] = $unserialize;
+                            }else{
+                                $meta_data[$field[1]] = $string;
+                            }
+                            
                         }
                     }
                     foreach( $meta_data as $k => $v ) {
